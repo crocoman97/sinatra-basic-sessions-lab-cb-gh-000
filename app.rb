6fa387b94ea421a2387653095c6c646614020766
Rauof M <abdelrauof197@gmail.com>
2018-02-19 06:42:11 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -1,4 +1,15 @@
 require_relative 'config/environment'
 
 class App < Sinatra::Base
+  
+  get '/' do 
+    enable :sessions
+    set :session_secret, "str"
+    erb :index
+  end 
+  
+  post '/checkout' do 
+    
+  end
+  
 end
