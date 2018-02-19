f5a3764b37313e2ff0400d2193e0654699b95e5a
Rauof M <abdelrauof197@gmail.com>
2018-02-19 06:42:55 +0000

    automatically backed up by learn

--- app.rb
+++ app.rb
@@ -2,6 +2,11 @@ require_relative 'config/environment'
 
 class App < Sinatra::Base
   
+  configure do 
+    enable sessions 
+    set session_secret, "str"
+  end
+  
   get '/' do 
     enable :sessions
     set :session_secret, "str"
